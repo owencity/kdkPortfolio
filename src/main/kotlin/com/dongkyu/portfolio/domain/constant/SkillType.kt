4b06e92690b6e345ff7097a824a1ba709c22a6ba
owencity <skyko6530@gmail.com>
2024-08-21 23:44:49 +0900

    kotlin entity , repository 추가

---
+++ src/main/kotlin/com/dongkyu/portfolio/domain/constant/SkillType.kt
@@ -0,0 +1,5 @@
+package com.dongkyu.portfolio.domain.constant
+
+enum class SkillType {
+    LANGUAGE, FREAMPEWORK, DATABASE, TOOL
+}
---
+++ src/main/kotlin/com/dongkyu/portfolio/domain/entity/BaseEntity.kt
@@ -0,0 +1,8 @@
+package com.dongkyu.portfolio.domain.entity
+
+import jakarta.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseEntity {
+
+}
